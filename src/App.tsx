90fe8b994d9516ff0a76cd41383d9715ff034bf8
snntaylan <sinan.taylan@outlook.com>
2026-04-25 16:09:39 +0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">hi</div>
+      </section>
+    </>
+  )
+}
+
+export default App
